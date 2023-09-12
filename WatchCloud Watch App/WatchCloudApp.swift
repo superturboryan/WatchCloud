68c44666cc4e867142cb7c78c9d427ca92033ee8
Ryan Forsyth <forsyth.r@gmail.com>
2023-09-12 11:49:47 +0100

    Initial Commit

---
+++ WatchCloud Watch App/WatchCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchCloudApp.swift
+//  WatchCloud Watch App
+//
+//  Created by Ryan Forsyth on 2023-09-12.
+//
+
+import SwiftUI
+
+@main
+struct WatchCloud_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
