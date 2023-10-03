4db98046be760f67789a0e65bfea60606ede6eef
Ryan Forsyth <forsyth.r@gmail.com>
2023-10-03 22:17:30 +0100

    UserDetailView: add playlist sections, loading

--- WatchCloud Watch App/UI/UserDetailView.swift
+++ WatchCloud Watch App/UI/UserDetailView.swift
@@ -13,37 +13,76 @@ struct UserDetailView: View {
     @EnvironmentObject var sc: SoundCloud
     @Binding var user: User
     
+    @State var isLoading = false
+    @State var tracks: [Track] = []
+    @State var likedTracks: [Track] = []
+    
     var isFollowed: Bool {
         (sc.usersImFollowing?.items ?? []).contains(user)
     }
     
     var body: some View {
         ScrollView {
-            LazyVStack {
+            VStack(spacing: 8) {
                 summary
+                if isLoading {
+                    loadingView
+                }
+                if !tracks.isEmpty {
+                    playlistSection("Top tracks", tracks.sorted(by: { $0.playbackCount ?? 0 > $1.playbackCount ?? 0 }))
+                    playlistSection("Most recent", tracks.sorted(by: { $0.createdAt > $1.createdAt }))
+                }
+                if !likedTracks.isEmpty {
+                    playlistSection("Liked tracks", likedTracks)
+                }
             }
             .padding(.top, -20)
             .padding(.horizontal)
             .fontDesign(.rounded)
+        }
+        .task {
+            if tracks.isEmpty && likedTracks.isEmpty {
+                await loadTracks()
+            }
         }
         .toolbar {
             followButton
         }
     }
     
+    @MainActor
+    private func loadTracks() async {
+        isLoading = true
+        do {
+            tracks = try await sc.getTracksForUser(user.id, 1000).items
+            likedTracks = try await sc.getLikedTracksForUser(user.id, 1000).items
+        } catch {
+            print("❌ Failed to load tracks for user")
+        }
+        isLoading = false
+    }
+    
+    private var loadingView: some View {
+        VStack(spacing: 8) {
+            ProgressView()
+                .tint(.scOrange)
+            Text("Loading tracks...")
+                .font(.footnote)
+                .foregroundColor(.secondary)
+        }
+        .padding(.vertical, 10)
+    }
+    
     private var summary: some View {
-        GeometryReader { geo in
-            VStack(spacing: 10) {
-                CachedImageView(url: user.largerAvatarUrl)
-                    .frame(width: geo.size.width * 0.6, height: geo.size.width * 0.6)
-                    .clipShape(Circle())
-                    .overlay(alignment: .bottom) {
-                        subscriptionLabel
-                    }
+        VStack(spacing: 10) {
+            CachedImageView(url: user.largerAvatarUrl)
+                .frame(width: 100, height: 100)
+                .clipShape(Circle())
+                .overlay(alignment: .bottom) {
+                    subscriptionLabel
+                }
 
-                artistInfoLabels
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            artistInfoLabels
         }
     }
     
@@ -70,6 +109,7 @@ struct UserDetailView: View {
             .font(.footnote)
             .minimumScaleFactor(0.8)
         }
+        .lineLimit(1)
     }
     
     private var followButton: some ToolbarContent {
@@ -96,22 +136,31 @@ struct UserDetailView: View {
     }
     
     private func playlistSection(_ title: String, _ tracks: [Track]) -> some View {
-        Text("")
+        VStack {
+            HStack {
+                Text(title)
+                Spacer()
+                Text(String(localized: "View all"))
+                    .foregroundStyle(.secondary)
+            }
+            .font(.footnote)
+            .onTapGesture {
+                // Go to PlaylistView
+            }
+            ForEach(Array(tracks.prefix(3))) { track in
+                TrackCellView(
+                    track: .constant(track),
+                    isPlaying: sc.loadedTrack == track,
+                    isDownloaded: sc.downloadedTracks.contains(track)
+                )
+            }
+        }
     }
 }
 
 #Preview {
     NavigationStack {
-        UserDetailView(user: .constant(testUser()))
+        UserDetailView(user: .constant(testUser(27127117)))
             .environmentObject(testSC)
     }
 }
-
-/**
- Avatar
- Username
- Follower count
- Top tracks (by playcount)
- Latest tracks (by date posted)
- Liked tracks
- */
