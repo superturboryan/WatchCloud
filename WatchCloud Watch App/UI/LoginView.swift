c9ffff07e2326c92e9f1865a9a6d33258ddac977
Ryan Forsyth <forsyth.r@gmail.com>
2023-10-09 22:06:55 +0100

    LoginView: adjust login button padding, minimumScaleFactor

--- WatchCloud Watch App/UI/LoginView.swift
+++ WatchCloud Watch App/UI/LoginView.swift
@@ -48,10 +48,10 @@ struct LoginView: View {
             Label(String(localized: "Connect", comment: "Verb"), systemImage: "cloud.fill")
                 .lineLimit(1)
                 .fontWeight(.semibold)
-                .minimumScaleFactor(0.8)
+                .minimumScaleFactor(0.7)
         }
         .font(.title3)
-        .padding(.horizontal, 24)
+        .padding(.horizontal, 20)
         .padding(.vertical, 18)
         .background(.white.opacity(0.12))
         .overlay {
