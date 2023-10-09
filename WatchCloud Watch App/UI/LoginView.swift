75f39fd6defefefb5402e537d039f430a2d3cbff
Ryan Forsyth <forsyth.r@gmail.com>
2023-10-09 14:22:10 +0100

    LoginView: refactor UI into separate views

--- WatchCloud Watch App/UI/LoginView.swift
+++ WatchCloud Watch App/UI/LoginView.swift
@@ -20,46 +20,8 @@ struct LoginView: View {
 
     var body: some View {
         VStack {
-            if #available(watchOS 10, *) {
-                TipView(CaptchaNotAppearingTip(), arrowEdge: .bottom).onAppear {
-                    showingTip = true
-                }.onDisappear {
-                    showingTip = false
-                }
-                .animation(.default, value: showingTip)
-            }
-            
-            Button {
-                Task {
-                    do {
-                        try await sc.login()
-                        AnalyticsManager.shared.log(.loginSuccess)
-                    } catch(SoundCloud.Error.cancelledLogin) {
-                        AnalyticsManager.shared.log(.loginCancelled)
-                        if #available(watchOS 10, *) {
-                            LoginView.hasTriedToLoginAndCancelled = true
-                        }
-                    } catch {
-                        showErrorAlert = true
-                        AnalyticsManager.shared.log(.loginFailure)
-                    }
-                }
-            } label: {
-                Label(String(localized: "Connect", comment: "Verb"), systemImage: "cloud.fill")
-                    .lineLimit(1)
-                    .fontWeight(.semibold)
-                    .minimumScaleFactor(0.8)
-            }
-            .font(.title3)
-            .padding(.horizontal, 24)
-            .padding(.vertical, 18)
-            .background(.white.opacity(0.12))
-            .overlay {
-                Capsule()
-                    .strokeBorder(style: StrokeStyle(lineWidth: 6))
-                    .foregroundStyle(LinearGradient.scOrange(.horizontal))
-            }
-            .clipShape(Capsule())
+            captchaNotAppearingTip
+            loginButton
         }
         .fullWidthAndHeight()
         .overlay(alignment: .bottom) {
@@ -79,6 +41,56 @@ struct LoginView: View {
         .ignoresSafeArea()
         .navigationTitle(String.empty)
         .interactiveDismissDisabled()
+    }
+    
+    private var loginButton: some View {
+        Button { tappedLogin() } label: {
+            Label(String(localized: "Connect", comment: "Verb"), systemImage: "cloud.fill")
+                .lineLimit(1)
+                .fontWeight(.semibold)
+                .minimumScaleFactor(0.8)
+        }
+        .font(.title3)
+        .padding(.horizontal, 24)
+        .padding(.vertical, 18)
+        .background(.white.opacity(0.12))
+        .overlay {
+            Capsule()
+                .strokeBorder(style: StrokeStyle(lineWidth: 6))
+                .foregroundStyle(LinearGradient.scOrange(.horizontal))
+        }
+        .clipShape(Capsule())
+    }
+    
+    private func tappedLogin() {
+        Task {
+            do {
+                try await sc.login()
+                AnalyticsManager.shared.log(.loginSuccess)
+            } catch(SoundCloud.Error.cancelledLogin) {
+                AnalyticsManager.shared.log(.loginCancelled)
+                if #available(watchOS 10, *) {
+                    LoginView.hasTriedToLoginAndCancelled = true
+                }
+            } catch {
+                showErrorAlert = true
+                AnalyticsManager.shared.log(.loginFailure)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var captchaNotAppearingTip: some View {
+        if #available(watchOS 10, *) {
+            TipView(CaptchaNotAppearingTip(), arrowEdge: .bottom).onAppear {
+                showingTip = true
+            }.onDisappear {
+                showingTip = false
+            }
+            .animation(.default, value: showingTip)
+        } else {
+            EmptyView()
+        }
     }
 }
 
