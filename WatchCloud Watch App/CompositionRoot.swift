52db04aa0628772ef338a9270f4cfa47aee6534e
Ryan Forsyth <forsyth.r@gmail.com>
2023-10-18 20:58:47 +0100

    CompositionRoot: add audioStore + userStore

--- WatchCloud Watch App/CompositionRoot.swift
+++ WatchCloud Watch App/CompositionRoot.swift
@@ -8,24 +8,34 @@
 import Foundation
 import SoundCloud
 
+/// Simplified dependency injection container
+///
+/// Use computed var to get new instances instead of singleton
+///
+/// Use private for dependencies that should be injected inside CompositionRoot, not accessed directly
 enum CompositionRoot {
-    // Use static let for singletons
-    // Use computed static var to get new instances instead of singleton
-    // Use closures to inject dependencies 
-    // Use private for dependencies that should be injected inside CompositionRoot, not accessed directly
     
+    // UI
     static let rootView = RootView()
     
-    @MainActor
-    static let scAudioPlayer = AudioPlayer(sc)
+    // Stores
+    static let audioStore = AudioStore(soundCloudService)
+    static let userStore = UserStore(soundCloudService)
     
-    @MainActor
-    static let sc = SoundCloud(config)
+    // Services
+    private static let soundCloudService = SoundCloudService(config)
     
+    // Config
     private static let config = SoundCloudConfig(
         apiURL: Config.apiUrl,
         clientId: Config.clientId,
         clientSecret: Config.clientSecret,
         redirectURI: Config.redirectURI
     )
+    
+    @MainActor
+    static let scAudioPlayer = AudioPlayer(sc)
+    
+    @MainActor
+    static let sc = SoundCloud(config)
 }
