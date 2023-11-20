5829d85495bc278c94a7400141a3bc5c257d2716
Ryan Forsyth <forsyth.r@gmail.com>
2023-11-20 14:49:55 +0000

    Notification: add loadedNowPlayingInfo

--- WatchCloud Watch App/Extension/Notification.swift
+++ WatchCloud Watch App/Extension/Notification.swift
@@ -8,6 +8,10 @@
 import Foundation
 
 extension Notification.Name {
+    
+    // AudioPlayer
+    static let loadedNowPlayingInfo = Notification.Name("loadedNowPlayingInfo")
+    
     // RootView
     static let switchToPlayerTab = Notification.Name("switchToPlayerTab")
     static let performLogout = Notification.Name("performLogout")
