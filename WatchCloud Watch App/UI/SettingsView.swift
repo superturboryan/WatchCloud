781e4cb71ec85424d1bc56c8b58f03de3b0ee939
Ryan Forsyth <forsyth.r@gmail.com>
2023-11-26 13:36:27 +0000

    SettingsView: only display downloads section if enabled, refactor UI

--- WatchCloud Watch App/UI/SettingsView.swift
+++ WatchCloud Watch App/UI/SettingsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct SettingsView: View {
     
+    @EnvironmentObject var userStore: UserStore
     @EnvironmentObject var audioStore: AudioStore
     
     @State var showDeleteAllAlert = false
@@ -20,34 +21,8 @@ struct SettingsView: View {
     
     var body: some View {
         List {
-            Section(
-                footer: Text("Displays a QR code in place of song artwork when watch is dimmed")
-            ) {
-                Toggle(isOn: Config.$showQRWhenWatchIsDimmed) {
-                    VStack {
-                        Text(String(localized: "Show QR code", comment: "Toggle label"))
-                    }
-                }
-            }
-            
-            if !audioStore.downloadedTracks.isEmpty {
-                Section(
-                    header: Text("Downloads")
-                ) {
-                    VStack(alignment: .leading, spacing: 8) {
-                        HStack {
-                            Text("\(audioStore.downloadedTracks.count) tracks")
-                            Text("(\(audioStore.downloadedTracksFileSize()) MB)").foregroundStyle(.secondary)
-                        }
-                        
-                        Button("Delete all", role: .destructive) {
-                            showDeleteAllAlert = true
-                        }
-                        .buttonStyle(.bordered)
-                    }
-                    .listRowBackground(Color.clear)
-                }
-            }
+            playbackSection
+            downloadsSection
         }
         .alert("Are you sure you want delete all downloads?", isPresented: $showDeleteAllAlert) {
             Button("Logout", role: .destructive) { try? audioStore.removeAllDownloads() }
@@ -56,11 +31,46 @@ struct SettingsView: View {
         .navigationBarTitle("Settings")
         .navigationBarTitleDisplayMode(.inline)
     }
+    
+    private var playbackSection: some View {
+        Section(
+            footer: Text("Display a QR code in place of song artwork when watch is dimmed")
+        ) {
+            Toggle(isOn: Config.$showQRWhenWatchIsDimmed) {
+                Text(String(localized: "Show QR code", comment: "Toggle label"))
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var downloadsSection: some View {
+        if Config.isDownloadingEnabled(for: userStore.myUser?.id) {
+            Section(
+                header: Text("Downloads")
+            ) {
+                HStack {
+                    Text("\(audioStore.downloadedTracks.count) tracks")
+                    Spacer()
+                    Text("(\(audioStore.downloadedTracksFileSize()) MB)").foregroundStyle(.secondary)
+                }
+                .animation(.default, value: audioStore.downloadedTracks)
+                
+                if !audioStore.downloadedTracks.isEmpty {
+                    Button("Delete all", role: .destructive) {
+                        showDeleteAllAlert = true
+                    }
+                    .buttonStyle(.bordered)
+                    .listItemTint(.clear)
+                }
+            }
+        }
+    }
 }
 
 #Preview {
     NavigationView {
         SettingsView()
             .environmentObject(AudioStore(testSC))
+            .environmentObject(UserStore(testSC))
     }
 }
