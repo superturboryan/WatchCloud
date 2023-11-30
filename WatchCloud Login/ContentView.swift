194455d8c36e721c0e9b13d54c224591ee59b8a6
Ryan Forsyth <forsyth.r@gmail.com>
2023-11-30 14:16:51 +0000

    Project: add empty iOS app target. Set min iOS to 15.0

---
+++ WatchCloud Login/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchCloud Login
+//
+//  Created by Ryan Forsyth on 2023-11-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchCloud Login/WatchCloud_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchCloud_LoginApp.swift
+//  WatchCloud Login
+//
+//  Created by Ryan Forsyth on 2023-11-30.
+//
+
+import SwiftUI
+
+@main
+struct WatchCloud_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
