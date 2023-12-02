a6f8725c3254c2ac51030c7552f659319ab32730
Ryan Forsyth <forsyth.r@gmail.com>
2023-12-02 21:49:13 +0000

    HelpView: add expandable list

--- WatchCloud Login/UI/HelpView.swift
+++ WatchCloud Login/UI/HelpView.swift
@@ -7,9 +7,50 @@
 
 import SwiftUI
 
+struct HelpItem: Hashable {
+    let title: String
+    let text: [String]
+}
+
 struct HelpView: View {
+    @State var helpItems: [HelpItem] = [
+        HelpItem(title: "Can I delete this app?", text: ["Yes!"]),
+        HelpItem(title: "What does it do?", text: ["Yes!", "Yes!", "Yes!"]),
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                List(helpItems, id: \.self) { item in
+                    ExpandableSection {
+                        ForEach(item.text, id: \.self) { text in
+                            Text(text)
+                        }
+                    } header: {
+                        Text(item.title)
+                            .font(.headline)
+                    }
+                }
+                .listStyle(.sidebar)
+            }
+            .navigationTitle("Help")
+        }
+    }
+}
+
+struct ExpandableSection<Content: View, Header: View>: View {
+    
+    @State var isExpanded = false
+    
+    let content: () -> Content
+    let header: () -> Header
+    
+    var body: some View {
+        if #available(iOS 17.0, *) {
+            Section(isExpanded: $isExpanded, content: content, header: header)
+        } else {
+            Section(content: content, header: header)
+        }
     }
 }
 
