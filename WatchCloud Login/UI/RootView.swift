1ec08b97a34ed59194302e3e1f31c1f581e60702
Ryan Forsyth <forsyth.r@gmail.com>
2023-12-03 23:22:35 +0000

    iOS RootView: refactor instructions list

--- WatchCloud Login/UI/RootView.swift
+++ WatchCloud Login/UI/RootView.swift
@@ -15,7 +15,9 @@ struct RootView: View {
     @State var isWatchAppInstalled = false
     @State var showAboutView = false
     @State var showSplashScreen = true
-        
+    
+    private let disabledOpacity = 0.4
+    
     @Namespace var header
     
     var body: some View {
@@ -28,7 +30,6 @@ struct RootView: View {
                 }
             }
             .animation(.default, value: showSplashScreen)
-            .fullWidthAndHeight()
             .sheet(isPresented: $showAboutView) {
                 AboutView()
             }
@@ -44,6 +45,7 @@ struct RootView: View {
     private var splashScreen: some View {
         headerView
             .task {
+                // Hide after delay
                 try? await Task.sleep(for: .seconds(0.75))
                 showSplashScreen = false
             }
@@ -88,70 +90,74 @@ struct RootView: View {
     
     @ViewBuilder
     private var instructionsView: some View {
-        let disabledOpacity = 0.4
-        
         VStack(spacing: 60) {
-            
-            // 1
-            VStack(spacing: 16) {
-                Label("Install WatchCloud app on Apple Watch", systemImage: "1.circle")
-                    .opacity(isWatchAppInstalled ? disabledOpacity : 1)
-                HStack {
-                    Image(systemName: isWatchAppInstalled ? "checkmark.circle.fill" : "exclamationmark.triangle.fill")
-                        .foregroundStyle(isWatchAppInstalled ? .green : .scOrangeLight)
-                        .symbolReplaceEffect()
-                    if isWatchAppInstalled {
-                        Text("Watch App Installed")
-                    } else {
-                        Text("Watch App Not Installed")
-                    }
-                }
-                .padding()
-                .background(.primary.opacity(0.12))
-                .clipShape(RoundedRectangle(cornerRadius: 8))
-            }
-            .animation(.default, value: isWatchAppInstalled)
-            
-            // 2
-            VStack(spacing: 16) {
-                Label("Connect to SoundCloud", systemImage: "2.circle")
-                    .opacity(isWatchAppInstalled && !authStore.isLoggedIn ? 1 : disabledOpacity)
-                
-                ZStack {
-                    if authStore.isLoggedIn {
-                        HStack {
-                            Image(systemName: "checkmark.circle.fill")
-                                .foregroundStyle(.green)
-                            Text("Account Connected")
-                        }
-                        .padding()
-                        .background(.primary.opacity(0.12))
-                        .clipShape(RoundedRectangle(cornerRadius: 8))
-                        .zIndex(1)
-                    } else {
-                        LoginButton {
-                            performLoginAndSendTokens()
-                        }
-                        .opacity(isWatchAppInstalled ? 1 : disabledOpacity)
-                        .disabled(!isWatchAppInstalled)
-                        .zIndex(0)
-                    }
-                }
-                .animation(.default, value: authStore.isLoggedIn)
-            }
-            
-            // 3
-            VStack(spacing: 16) {
-                Label("Open app on Apple Watch", systemImage: "3.circle")
-                appIcon
-                    .frame(width: 50, height: 50)
-                    .clipShape(Circle())
-            }
-            .opacity(authStore.isLoggedIn ? 1 : disabledOpacity)
-            .animation(.default, value: authStore.isLoggedIn)
+            installWatchOSApp // 1
+            connectToSoundCloud // 2
+            openWatchOSApp // 3
         }
         .font(.headline)
         .fontWeight(.bold)
+    }
+    
+    private var installWatchOSApp: some View {
+        VStack(spacing: 16) {
+            Label("Install WatchCloud app on Apple Watch", systemImage: "1.circle")
+                .opacity(isWatchAppInstalled ? disabledOpacity : 1)
+            HStack {
+                Image(systemName: isWatchAppInstalled ? "checkmark.circle.fill" : "exclamationmark.triangle.fill")
+                    .foregroundStyle(isWatchAppInstalled ? .green : .scOrangeLight)
+                    .symbolReplaceEffect()
+                if isWatchAppInstalled {
+                    Text("Watch App Installed")
+                } else {
+                    Text("Watch App Not Installed")
+                }
+            }
+            .padding()
+            .background(.primary.opacity(0.12))
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+        }
+        .animation(.default, value: isWatchAppInstalled)
+    }
+    
+    private var connectToSoundCloud: some View {
+        VStack(spacing: 16) {
+            Label("Connect to SoundCloud", systemImage: "2.circle")
+                .opacity(isWatchAppInstalled && !authStore.isLoggedIn ? 1 : disabledOpacity)
+            
+            ZStack {
+                if authStore.isLoggedIn {
+                    HStack {
+                        Image(systemName: "checkmark.circle.fill")
+                            .foregroundStyle(.green)
+                        Text("Account Connected")
+                    }
+                    .padding()
+                    .background(.primary.opacity(0.12))
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .zIndex(1)
+                } else {
+                    LoginButton {
+                        performLoginAndSendTokens()
+                    }
+                    .opacity(isWatchAppInstalled ? 1 : disabledOpacity)
+                    .disabled(!isWatchAppInstalled)
+                    .zIndex(0)
+                }
+            }
+            .animation(.default, value: authStore.isLoggedIn)
+        }
+    }
+    
+    private var openWatchOSApp: some View {
+        VStack(spacing: 16) {
+            Label("Open app on Apple Watch", systemImage: "3.circle")
+            appIcon
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+        }
+        .opacity(authStore.isLoggedIn ? 1 : disabledOpacity)
+        .animation(.default, value: authStore.isLoggedIn)
     }
     
     private var appIcon: some View {
